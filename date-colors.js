0ea9c8efd817aad17710c5afd7f064ad4f65b2be
charliekozey <charkoz@mailbox.org>
2023-02-20 14:23:03 -0500

    more pruning

--- date-colors.js
+++
@@ -1,8 +0,0 @@
-// const links = document.getElementsByTagName("a")
-// const today = new Date()
-// const colors = ["red", "orange", "yellow", "green", "blue", "purple", "green"]
-
-// for (link of links) {
-//     link.style.color = colors[today.getDay()]
-//     link.style.backgroundColor = colors[today.getDay()]
-// } 
